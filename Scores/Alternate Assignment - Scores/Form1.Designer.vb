1d320e9334f519a61093ff2089c3357e7c1f1ef7
Roshaan Q <49801279+PourquoiEstTu@users.noreply.github.com>
2022-01-08 16:07:14 -0500

    Add files via upload

---
+++ Scores/Alternate Assignment - Scores/Form1.Designer.vb
@@ -0,0 +1,64 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGetScore = New System.Windows.Forms.Button()
+        Me.lstScores = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'btnGetScore
+        '
+        Me.btnGetScore.Font = New System.Drawing.Font("Consolas", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGetScore.Location = New System.Drawing.Point(12, 12)
+        Me.btnGetScore.Name = "btnGetScore"
+        Me.btnGetScore.Size = New System.Drawing.Size(200, 38)
+        Me.btnGetScore.TabIndex = 0
+        Me.btnGetScore.Text = "Get Score"
+        Me.btnGetScore.UseVisualStyleBackColor = True
+        '
+        'lstScores
+        '
+        Me.lstScores.Font = New System.Drawing.Font("Consolas", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstScores.FormattingEnabled = True
+        Me.lstScores.ItemHeight = 15
+        Me.lstScores.Location = New System.Drawing.Point(12, 56)
+        Me.lstScores.Name = "lstScores"
+        Me.lstScores.Size = New System.Drawing.Size(200, 169)
+        Me.lstScores.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(226, 241)
+        Me.Controls.Add(Me.lstScores)
+        Me.Controls.Add(Me.btnGetScore)
+        Me.Name = "Form1"
+        Me.Text = "(Insert Sport Here) Teams Scores"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnGetScore As Button
+    Friend WithEvents lstScores As ListBox
+End Class
---
+++ Scores/Alternate Assignment - Scores/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    'Roshaan Q., Nov. 12, 2020, A make-up program for file access and formatting; a program for getting the scores of different teams and averaging them out
+    Private Sub btnGetScore_Click(sender As Object, e As EventArgs) Handles btnGetScore.Click
+        Dim reader As System.IO.StreamReader
+        Dim line As String
+        Dim teamScores() As String 'dynamic array
+        Dim stringAverage As String 'created for number to string formatting
+        Dim totalPointsAverage As Double
+        Dim totalLeft As Integer
+        Dim totalRight As Integer
+
+        lstScores.Items.Clear()
+        lstScores.Items.Add(String.Format("{0,-10}{1,13}", "Winners", "Total Points"))
+        lstScores.Items.Add(String.Format("{0, -23}", "======================="))
+
+        Try
+            reader = My.Computer.FileSystem.OpenTextFileReader(Application.StartupPath & "\scores.csv")
+            Do Until reader.EndOfStream
+                totalRight = 0
+                totalLeft = 0 'if they aren't reset, they will make ridiculously huge numbers
+                line = reader.ReadLine
+                teamScores = line.Split(",")
+                For x = 1 To 5 'loops through first team's scores
+                    totalLeft += Val(teamScores(x))
+                Next
+                For x = 7 To 11 'loops through second (right) team's scores
+                    totalRight += Val(teamScores(x))
+                Next
+                If totalLeft > totalRight Then 'if statement for deciding which team's total scores to display
+                    lstScores.Items.Add(String.Format("{0,-16}{1,7}", teamScores(0), totalLeft))
+                    totalPointsAverage += totalLeft 'collects values to calculate average
+                Else
+                    lstScores.Items.Add(String.Format("{0,-16}{1,7}", teamScores(6), totalRight))
+                    totalPointsAverage += totalRight 'collects values to calculate average
+                End If
+            Loop
+            totalPointsAverage /= 5 'final average calculation as double/number
+            stringAverage = totalPointsAverage.ToString("0.00") 'string conversion
+            lstScores.Items.Add(" ")
+            lstScores.Items.Add(String.Format("{0,-16}", "Average Total Score:"))
+            lstScores.Items.Add(String.Format("{0,-16}", stringAverage))
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Class
